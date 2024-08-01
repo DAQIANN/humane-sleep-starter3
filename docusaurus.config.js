8b00ae0475533053b70e18ed3a0def2e7b34470a
Daniel Qian <danielqian@Daniels-MacBook-Pro-2.local>
2024-07-31 23:29:44 -0700

    Attempting to keep all dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -124,7 +124,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
+        theme: darkCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
